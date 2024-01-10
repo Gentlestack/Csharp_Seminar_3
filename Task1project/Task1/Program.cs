788af00c22ff2f75be2f9c3df31e1a8d65b5c0ab
Gentlestack <valera_dyshlovoy@mail.ru>
2024-01-10 21:53:31 +0400

    Initial commit

---
+++ Task1project/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿int M = 1;
+int N = 10;
+
+Console.WriteLine();
+void WriteNumbers(int a, int b)
+{
+    a = a + 1;
+
+    if (a != b)
+    {
+        Console.WriteLine(a);
+        WriteNumbers(a, b);
+    }
+    Console.WriteLine("Ура, наконец то эта ебливая рекурсия закончилась");
+}
+
+WriteNumbers(M, N);
+
---
+++ Task2project/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int M = 1;
+int N = 10;
+
+int LeviAccerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    if ((m > 0) && (n == 0))
+    {
+        return LeviAccerman(m - 1, 1);
+    }
+    else
+    {
+        return LeviAccerman(m - 1, LeviAccerman(m, n - 1));
+    }
+}
+
+Console.WriteLine(LeviAccerman(M, N));
